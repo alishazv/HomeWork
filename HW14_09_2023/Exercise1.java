60bd3012c2ea0f8f665168738d230699d2131314
alina zvolinskaya(is12) <alinazzz111555@gmail.com>
2023-09-14 22:06:51 +1000

    hw done

---
+++ HW14_09_2023/Exercise1.java
@@ -0,0 +1,11 @@
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println(" /\\_/\\     -----");
+        System.out.println("( ' ' )  / Hello \\");
+        System.out.println("(  -  ) <  Junior |");
+        System.out.println(" | | |   \\ Coder!/");
+        System.out.println(" (_|_)     -----");
+
+
+    }
+}
---
+++ HW14_09_2023/Exercise2version2large.java
@@ -0,0 +1,14 @@
+public class Exercise2version2large {
+    public static void main(String[] args) {
+        System.out.println("   /  /  /  /  / ");
+        System.out.println(" +'' '' '' '' ''+");
+        System.out.println(" (|             |)");
+        System.out.println("( |    0   0    | )");
+        System.out.println(" (|             |)");
+        System.out.println("  |      ^      |");
+        System.out.println("  |             |");
+        System.out.println("  |    ' _ '    |");
+        System.out.println("  |             |");
+        System.out.println("  + - - - - - - +");
+    }
+}
